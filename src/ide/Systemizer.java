bfe7f7684e5a39136924b44bf1e132038bd4e49c
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2017-08-11 12:40:20 -0700

    update systemizer.java

--- src/ide/Systemizer.java
+++ src/ide/Systemizer.java
@@ -1,8 +1,18 @@
 package ide;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Systemizer {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.printf("This is the classpath:  %s %n",
+                System.getProperty("java.class.path"));
+        Set<String> propNames = new TreeSet<String>(System.getProperties().stringPropertyNames());
+        for (String propertyName : propNames) {
+            System.out.printf("%s is %s %n",
+                    propertyName,
+                    System.getProperty(propertyName));
+        }
     }
 }
